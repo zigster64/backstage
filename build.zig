d3ef6b8cc9d17936aa3326c36ea3e2c6a09a58dc
Thom <thomvanoorschot@hotmail.com>
2025-02-18 18:54:22 +0100

    Renamed

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "fabula",
+        .name = "alphazig",
         .root_source_file = .{ .cwd_relative = "src/main.zig" },
         .target = target,
         .optimize = optimize,
