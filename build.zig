24564bd71f308ef32bb58b710a5758cce564c8a8
Thom <thomvanoorschot@hotmail.com>
2025-03-15 18:00:28 +0100

    Changed path

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     // Create a **Static Library** for Alphazig
     const lib = b.addStaticLibrary(.{
         .name = "alphazig",
-        .root_source_file = .{ .cwd_relative = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"), 
         .target = target,
         .optimize = optimize,
     });
