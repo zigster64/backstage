cb545cb04651c0667d5d50a465d8960ac5e4647b
Thom <thomvanoorschot@hotmail.com>
2025-03-16 09:55:52 +0100

    attempt

--- build.zig
+++ build.zig
@@ -1,91 +1,88 @@
 const std = @import("std");
 
-const BuildContext = struct {
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.Mode,
-    lib_module: *std.Build.Module,
-    websocket_dep: *std.Build.Dependency,
-};
-
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "alphazig",
-        .root_source_file = .{ .cwd_relative = "src/root.zig" },
+    const alphazig_mod = b.addModule("alphazig", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
+
+    const alphazig_lib = try buildLibAlphaZig(b, .{
         .target = target,
         .optimize = optimize,
     });
-    lib.addIncludePath(b.path("lib/neco"));
-    lib.addIncludePath(b.path("lib/boot_neco"));
-    b.installArtifact(lib);
+    alphazig_mod.linkLibrary(alphazig_lib);
 
-    const websocket_dep = b.dependency("websocket", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const lib_module = b.addModule("alphazig", .{
-        .root_source_file = .{ .cwd_relative = "src/root.zig" },
-    });
-
-    const build_context = BuildContext{
-        .b = b,
-        .target = target,
-        .optimize = optimize,
-        .lib_module = lib_module,
-        .websocket_dep = websocket_dep,
-    };
     // Examples
     const examples = .{
         "example",
     };
 
     inline for (examples) |example| {
-        buildExample(b, example, build_context);
-    }
-
-    // Tests
-    const lib_tests = b.addTest(.{
-        .root_source_file = .{ .cwd_relative = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_tests = b.addRunArtifact(lib_tests);
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_lib_tests.step);
-
-    // Example tests
-    inline for (examples) |example| {
-        const example_tests = b.addTest(.{
-            .root_source_file = .{ .cwd_relative = "src/examples/" ++ example ++ ".zig" },
+        buildExample(b, example, .{
             .target = target,
             .optimize = optimize,
+            .alphazig_mod = alphazig_mod,
         });
-        example_tests.root_module.addImport("alphazig", lib_module);
-
-        const run_example_tests = b.addRunArtifact(example_tests);
-        const example_test_step = b.step("test-" ++ example, "Run " ++ example ++ " example tests");
-        example_test_step.dependOn(&run_example_tests.step);
-        test_step.dependOn(example_test_step);
     }
 }
 
-fn buildExample(b: *std.Build, comptime exampleName: []const u8, options: BuildContext) void {
+const LibOptions = struct {
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.Mode,
+};
+fn buildLibAlphaZig(b: *std.Build, options: LibOptions) !*std.Build.Step.Compile {
+    const lib = b.addStaticLibrary(.{
+        .name = "alphazig",
+        .target = options.target,
+        .optimize = options.optimize,
+        .link_libc = true,
+    });
+    lib.addIncludePath(b.path("lib/neco"));
+    lib.addIncludePath(b.path("lib/boot_neco"));
+        const necoCFlags = &.{
+        "-std=c11",
+        "-O0",
+        "-g3",
+        "-Wall",
+        "-Wextra",
+        "-fstrict-aliasing",
+        "-DLLCO_NOUNWIND",
+        "-pedantic",
+        "-Werror",
+        "-fno-omit-frame-pointer",
+    };
+    
+    lib.addIncludePath(b.path("lib/neco"));
+    lib.addCSourceFile(.{
+        .file = b.path("lib/neco/neco.c"),
+        .flags = necoCFlags,
+    });
+    b.installArtifact(lib);
+    return lib;
+}
+
+const ExampleOptions = struct {
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.Mode,
+    alphazig_mod: *std.Build.Module,
+};
+fn buildExample(b: *std.Build, comptime exampleName: []const u8, options: ExampleOptions) void {
     const exe = b.addExecutable(.{
         .name = "alphazig-" ++ exampleName,
         .root_source_file = .{ .cwd_relative = "src/examples/" ++ exampleName ++ ".zig" },
         .target = options.target,
         .optimize = options.optimize,
     });
-
-    exe.root_module.addImport("alphazig", options.lib_module);
-    exe.root_module.addImport("websocket", options.websocket_dep.module("websocket"));
+    const websocket_dep = b.dependency("websocket", .{
+        .target = options.target,
+        .optimize = options.optimize,
+    });
+    exe.root_module.addImport("alphazig", options.alphazig_mod);
+    exe.root_module.addImport("websocket", websocket_dep.module("websocket"));
     exe.linkSystemLibrary("c");
-    addNeco(b, exe, options.lib_module);
+    addNeco(b, exe, options.alphazig_mod);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -95,7 +92,6 @@ fn buildExample(b: *std.Build, comptime exampleName: []const u8, options: BuildC
 }
 
 fn addNeco(b: *std.Build, exe: *std.Build.Step.Compile, lib_module: *std.Build.Module) void {
-    // Neco - coroutines
     const necoCFlags = &.{
         "-std=c11",
         "-O0",
@@ -107,25 +103,12 @@ fn addNeco(b: *std.Build, exe: *std.Build.Step.Compile, lib_module: *std.Build.M
         "-pedantic",
         "-Werror",
         "-fno-omit-frame-pointer",
-        //"-fsanitize=address",
-        //"-Wall",
-        //"-Wextra",
-        //"-O0", // No optimizations at all (used for debugging bruh)...later remove this.
-
-        // RC: Some build errors are simply because compiler is too strict, need to loosen the error requirements.
-        //"-Wunused-parameter",
-        //"-Wzero-length-array",
     };
-    // Not sure if this is needed.
-    // exe.addIncludePath(b.path("lib/neco"));
-    // exe.addIncludePath(b.path("lib/boot_neco"));
+
     lib_module.addIncludePath(b.path("lib/neco"));
     lib_module.addIncludePath(b.path("lib/boot_neco"));
     exe.addCSourceFile(.{
         .file = b.path("lib/neco/neco.c"),
         .flags = necoCFlags,
     });
-
-    // Maybe needed?
-    // exe.defineCMacro("SCO_QUICKSTART", "1");
 }
